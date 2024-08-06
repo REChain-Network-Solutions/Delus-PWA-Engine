32af9bacb412cee8d87a70d74152c59e8141d467
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2024-08-07 00:28:59 +0300

    Initial release

---
+++ SngineWeb/SngineWebApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelusWebApp.swift
+//  DelusWeb
+//
+//  Created by Dmitry Sorokin
+//
+
+import SwiftUI
+
+@main
+struct SngineWebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
